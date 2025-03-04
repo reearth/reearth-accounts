7946a08ce0595f1c767d22257bd24422add96726
akiyatomohiro <thairwars@gmail.com>
2025-03-04 19:46:00 +0700

    fix resolver

--- server/internal/adapter/gql/resolver_cerbos.go
+++ server/internal/adapter/gql/resolver_cerbos.go
@@ -5,14 +5,20 @@ import (
 
 	"github.com/reearth/reearth-accounts/internal/adapter/gql/gqlmodel"
 	"github.com/reearth/reearth-accounts/internal/usecase/interfaces"
+	"github.com/reearth/reearthx/account/accountdomain/user"
 )
 
 func (r *queryResolver) CheckPermission(ctx context.Context, input gqlmodel.CheckPermissionInput) (*gqlmodel.CheckPermissionPayload, error) {
-	res, err := usecases(ctx).Cerbos.CheckPermission(ctx, interfaces.CheckPermissionParam{
+	userId, err := user.IDFrom(input.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := usecases(ctx).Cerbos.CheckPermission(ctx, userId, interfaces.CheckPermissionParam{
 		Service:  input.Service,
 		Resource: input.Resource,
 		Action:   input.Action,
-	}, getUser(ctx))
+	})
 	if err != nil {
 		return nil, err
 	}
