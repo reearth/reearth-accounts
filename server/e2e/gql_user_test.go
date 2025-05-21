93ed513e8d73b017002fc307f52adbd52f74306e
akiyatomohiro <thairwars@gmail.com>
2025-05-21 11:13:49 +0800

    reearth-accounts/server/e2e

--- server/e2e/gql_user_test.go
+++ server/e2e/gql_user_test.go
@@ -8,17 +8,17 @@ import (
 	"testing"
 
 	"github.com/reearth/reearth-accounts/internal/app"
-	"github.com/reearth/reearthx/account/accountdomain"
-	"github.com/reearth/reearthx/account/accountdomain/user"
-	"github.com/reearth/reearthx/account/accountdomain/workspace"
-	"github.com/reearth/reearthx/account/accountusecase/accountrepo"
+	"github.com/reearth/reearth-accounts/internal/usecase/repo"
+	"github.com/reearth/reearth-accounts/pkg/id"
+	"github.com/reearth/reearth-accounts/pkg/user"
+	"github.com/reearth/reearth-accounts/pkg/workspace"
 	"github.com/reearth/reearthx/idx"
 	"github.com/reearth/reearthx/rerror"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/text/language"
 )
 
-func baseSeederOneUser(ctx context.Context, r *accountrepo.Container) error {
+func baseSeederOneUser(ctx context.Context, r *repo.Container) error {
 	auth := user.ReearthSub(uId.String())
 	u := user.New().ID(uId).
 		Name("e2e").
@@ -38,10 +38,10 @@ func baseSeederOneUser(ctx context.Context, r *accountrepo.Container) error {
 
 	w := workspace.New().ID(wId).
 		Name("e2e").
-		Members(map[idx.ID[accountdomain.User]]workspace.Member{
+		Members(map[idx.ID[id.User]]workspace.Member{
 			uId: roleOwner,
 		}).
-		Integrations(map[idx.ID[accountdomain.Integration]]workspace.Member{
+		Integrations(map[idx.ID[id.Integration]]workspace.Member{
 			iId: roleOwner,
 		}).
 		MustBuild()
@@ -51,7 +51,7 @@ func baseSeederOneUser(ctx context.Context, r *accountrepo.Container) error {
 	return nil
 }
 
-func baseSeederUser(ctx context.Context, r *accountrepo.Container) error {
+func baseSeederUser(ctx context.Context, r *repo.Container) error {
 	auth := user.ReearthSub(uId.String())
 	u := user.New().ID(uId).
 		Name("e2e").
@@ -91,10 +91,10 @@ func baseSeederUser(ctx context.Context, r *accountrepo.Container) error {
 
 	w := workspace.New().ID(wId).
 		Name("e2e").
-		Members(map[idx.ID[accountdomain.User]]workspace.Member{
+		Members(map[idx.ID[id.User]]workspace.Member{
 			uId: roleOwner,
 		}).
-		Integrations(map[idx.ID[accountdomain.Integration]]workspace.Member{
+		Integrations(map[idx.ID[id.Integration]]workspace.Member{
 			iId: roleOwner,
 		}).
 		MustBuild()
@@ -104,11 +104,11 @@ func baseSeederUser(ctx context.Context, r *accountrepo.Container) error {
 
 	w2 := workspace.New().ID(wId2).
 		Name("e2e2").
-		Members(map[idx.ID[accountdomain.User]]workspace.Member{
+		Members(map[idx.ID[id.User]]workspace.Member{
 			uId:  roleOwner,
 			uId3: roleReader,
 		}).
-		Integrations(map[idx.ID[accountdomain.Integration]]workspace.Member{
+		Integrations(map[idx.ID[id.Integration]]workspace.Member{
 			iId: roleOwner,
 		}).
 		MustBuild()
