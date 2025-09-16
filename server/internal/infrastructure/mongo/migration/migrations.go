3480f592e3ca76af3fdd67569496f71080575716
Wilfred <wilfred.mulenga@gmail.com>
2025-09-16 15:00:37 +0900

    fix: add logger to view mongodb commands (#92)

--- server/internal/infrastructure/mongo/migration/migrations.go
+++ server/internal/infrastructure/mongo/migration/migrations.go
@@ -24,5 +24,5 @@ var migrations = migration.Migrations[DBClient]{
 	250911120003: AddCaseInsensitiveUserAliasIndex,
 	250911120004: AddCaseInsensitiveUserEmailIndex,
 	250911120005: AddCaseInsensitiveUserWorkspaceIndex,
-	250911120006: AddCaseInsensitiveUserSubsIndex, 
+	// 250911120006: AddCaseInsensitiveUserSubsIndex,
 }
