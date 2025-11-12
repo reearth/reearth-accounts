e5d7aed1f8e2ff683f8f9fa2c205b32b41df8e6d
soneda-yuya <soneda.yuya@gmail.com>
2025-11-12 18:21:03 +0900

    chore(server): add function for checking unauthorized error on each services

--- server/pkg/gqlclient/gqlerror/error.go
+++ server/pkg/gqlclient/gqlerror/error.go
@@ -13,6 +13,10 @@ type AccountsError error
 
 var ErrUnauthorized AccountsError = errors.New("unauthorized")
 
+func IsUnauthorized(err error) bool {
+	return strings.Contains(err.Error(), ErrUnauthorized.Error())
+}
+
 func ReturnAccountsError(ctx context.Context, err error) AccountsError {
 	_, file, line, _ := runtime.Caller(1)
 	log.Errorfc(ctx, "[Error] error with caller logging at %s:%d %+v", file, line, err)
