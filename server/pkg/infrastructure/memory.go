12c3e28bfe4030d7bee95893cf17c7ce7af31831
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-11-21 13:30:40 +0900

    add export package

---
+++ server/pkg/infrastructure/memory.go
@@ -0,0 +1,18 @@
+package infrastructure
+
+import (
+	"github.com/reearth/reearth-accounts/server/internal/infrastructure/memory"
+	"github.com/reearth/reearth-accounts/server/pkg/repo"
+)
+
+// NewMemoryUser creates a new in-memory User repository
+func NewMemoryUser() repo.User {
+	internal := memory.NewUser()
+	return NewUserAdapter(internal)
+}
+
+// NewMemoryWorkspace creates a new in-memory Workspace repository
+func NewMemoryWorkspace() repo.Workspace {
+	internal := memory.NewWorkspace()
+	return NewWorkspaceAdapter(internal)
+}
