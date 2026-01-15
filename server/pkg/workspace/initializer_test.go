81305f732ab746254df5b00efbe420f92c2bbde3
soneda-yuya <soneda.yuya@gmail.com>
2026-01-15 11:40:50 +0900

    revival test code

---
+++ server/pkg/workspace/initializer_test.go
@@ -0,0 +1,132 @@
+package workspace
+
+import (
+	"testing"
+
+	"github.com/reearth/reearth-accounts/server/pkg/role"
+	"github.com/reearth/reearth-accounts/server/pkg/user"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	uid := user.NewID()
+	tid := NewID()
+	expectedSub := user.Auth{
+		Provider: "###",
+		Sub:      "###",
+	}
+	tests := []struct {
+		Name, Email, Username string
+		Sub                   user.Auth
+		UID                   *user.ID
+		TID                   *ID
+		ExpectedUser          *user.User
+		ExpectedWorkspace     *Workspace
+		Err                   error
+	}{
+		{
+			Name:     "Success create user",
+			Email:    "xx@yy.zz",
+			Username: "nnn",
+			Sub: user.Auth{
+				Provider: "###",
+				Sub:      "###",
+			},
+			UID: &uid,
+			TID: &tid,
+			ExpectedUser: user.New().
+				ID(uid).
+				Email("xx@yy.zz").
+				Name("nnn").
+				Alias("nnn").
+				Workspace(tid).
+				Auths([]user.Auth{expectedSub}).
+				MustBuild(),
+			ExpectedWorkspace: New().
+				ID(tid).
+				Name("nnn").
+				Alias("nnn").
+				Members(map[user.ID]Member{uid: {Role: role.RoleOwner, Disabled: false, InvitedBy: uid}}).
+				Personal(true).
+				MustBuild(),
+			Err: nil,
+		},
+		{
+			Name:     "Success nil workspace id",
+			Email:    "xx@yy.zz",
+			Username: "nnn",
+			Sub: user.Auth{
+				Provider: "###",
+				Sub:      "###",
+			},
+			UID: &uid,
+			TID: nil,
+			ExpectedUser: user.New().
+				ID(uid).
+				Email("xx@yy.zz").
+				Name("nnn").
+				Alias("nnn").
+				Workspace(tid).
+				Auths([]user.Auth{expectedSub}).
+				MustBuild(),
+			ExpectedWorkspace: New().
+				NewID().
+				Name("nnn").
+				Alias("nnn").
+				Members(map[user.ID]Member{uid: {Role: role.RoleOwner, Disabled: false, InvitedBy: uid}}).
+				Personal(true).
+				MustBuild(),
+			Err: nil,
+		},
+		{
+			Name:     "Success nil id",
+			Email:    "xx@yy.zz",
+			Username: "nnn",
+			Sub: user.Auth{
+				Provider: "###",
+				Sub:      "###",
+			},
+			UID: nil,
+			TID: &tid,
+			ExpectedUser: user.New().
+				NewID().
+				Email("xx@yy.zz").
+				Name("nnn").
+				Alias("nnn").
+				Workspace(tid).
+				Auths([]user.Auth{expectedSub}).
+				MustBuild(),
+			ExpectedWorkspace: New().
+				ID(tid).
+				Name("nnn").
+				Alias("nnn").
+				Members(map[user.ID]Member{user.NewID(): {Role: role.RoleOwner, Disabled: false, InvitedBy: user.NewID()}}).
+				Personal(true).
+				MustBuild(),
+			Err: nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+			user, workspace, err := Init(InitParams{
+				Email:       tt.Email,
+				Name:        tt.Username,
+				Sub:         &tt.Sub,
+				UserID:      tt.UID,
+				WorkspaceID: tt.TID,
+			})
+			if tt.Err == nil {
+				assert.Equal(t, tt.ExpectedUser.Email(), user.Email())
+				assert.Equal(t, tt.ExpectedUser.Name(), user.Name())
+				assert.Equal(t, tt.ExpectedUser.Auths(), user.Auths())
+
+				assert.Equal(t, tt.ExpectedWorkspace.Name(), workspace.Name())
+				assert.Equal(t, tt.ExpectedWorkspace.IsPersonal(), workspace.IsPersonal())
+			} else {
+				assert.Equal(t, tt.Err, err)
+			}
+		})
+	}
+}
